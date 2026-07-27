e81577c44a72ffbaff50fd8912b042b4db8b3850
minagergeswaheeb2006 <minawaheeb272006@gmail.com>
2026-07-27 03:31:34 +0300

    Update portfolio with projects and responsive design

--- js.js
+++ js.js
@@ -1,1 +1,61 @@
+const slider = document.querySelector(".project-cards");
+slider.innerHTML += slider.innerHTML;
+slider.innerHTML += slider.innerHTML;
+slider.innerHTML += slider.innerHTML;
+let position = 0;
+const speed = 1;
 
+let pause = false;
+
+slider.addEventListener("mouseenter", () => {
+    pause = true;
+});
+
+slider.addEventListener("mouseleave", () => {
+    pause = false;
+});
+
+function animate() {
+
+    if (!pause) {
+
+        position -= speed;
+
+        if (Math.abs(position) >= slider.scrollWidth / 2) {
+            position = 0;
+        }
+
+        slider.style.transform = `translateX(${position}px)`;
+    }
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+let isDragging = false;
+let startX = 0;
+let startPosition = 0;
+
+slider.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX;
+    startPosition = position;
+    slider.style.cursor = "grabbing";
+});
+
+window.addEventListener("mouseup", () => {
+    isDragging = false;
+    slider.style.cursor = "grab";
+});
+
+window.addEventListener("mousemove", (e) => {
+
+    if (!isDragging) return;
+
+    const walk = e.pageX - startX;
+
+    position = startPosition + walk;
+
+    slider.style.transform = `translateX(${position}px)`;
+
+});
